dd8d9b92704d1bd8d2761575a0c39eaea8640cc1
adrianmontijo504 <adrianmontijo319@gmail.com>
2026-04-06 18:20:23 -0700

    Update maps-data.js

--- js/maps-data.js
+++ js/maps-data.js
@@ -13,8 +13,8 @@ window.MAP_GUIDES = [
     steps: [
       "Step 1 — Find 2 Telescope (Harmonic Oculus) components. (1st - Luminarium, 2nd - Machina Astralis).",
       "Step 2 — Go to Onservatory Dome and interact with Telescope to install Components."
-      "Step 3 - Defend Telescope for 45 seconds to unlock Pack-A-Punch."
-      "Step 4 - Collect Car Battery. (With a Pack-A-Punched weapon, shoot hood of Ol' Tessie)."
+      "Step 3 — Defend Telescope for 45 seconds to unlock Pack-A-Punch."
+      "Step 4 — Collect Car Battery. (With a Pack-A-Punched weapon, shoot hood of Ol' Tessie)."
     ]
   },
   {
